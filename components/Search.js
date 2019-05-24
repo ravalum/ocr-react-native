1ae678cd93281c4cde78ee97a420ce5f0687f594
Guillaume <guillaume.passebon@gmail.com>
2019-05-24 14:34:55 +0200

    film search + loading + pagination

--- components/Search.js
+++ components/Search.js
@@ -1,27 +1,85 @@
 // Components/Search.js
 
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
 import films from '../helpers/filmsData'
 import FilmItem from './FilmItem';
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
-  _loadFilms() {
-      getFilmsFromApiWithSearchedText("star").then(data => console.log(data));
+  constructor(props) {
+    super(props)
+    this.searchedText = ""
+    this.page = 0 // Compteur pour connaître la page courante
+    this.totalPages = 0 // Nombre de pages totales pour savoir si on a atteint la fin des retours de l'API TMDB
+    this.state = { 
+      films: [],
+      isLoading: false
+    }
   }
   
+  _searchTextInputChanged(text) {
+    this.searchedText = text
+  }
+
+  _searchFilms() {
+    // Ici on va remettre à zéro les films de notre state
+    this.page = 0
+    this.totalPages = 0
+    this.setState({
+      films: []
+    }, () => { 
+      this._loadFilms() 
+    })
+  }
+
+  _loadFilms() {
+    if(this.searchedText.length > 0){
+      this.setState({ isLoading: true }) 
+      getFilmsFromApiWithSearchedText(this.searchedText,this.page+1).then(data => {
+        this.page = data.page
+        this.totalPages = data.total_pages
+        this.setState({ 
+          films: [ ...this.state.films, ...data.results ],
+          isLoading: false
+        })
+      }); 
+    }  
+  }
+  
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+          {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+        </View>
+      )
+    }
+  }
+
   render() {
     return (
         <View style={styles.main_container}>
-        <TextInput style={styles.textinput} placeholder='Titre du film'/>
-        <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+        <TextInput 
+          style={styles.textinput} 
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+          onSubmitEditing={() => this._searchFilms()}/>
+        <Button title='Rechercher' onPress={() => this._searchFilms()}/>
         {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList */}
         <FlatList
-          data={films}
+          data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({item}) => <FilmItem film={item}/>}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+            if (this.page < this.totalPages) { // On vérifie qu'on n'a pas atteint la fin de la pagination (totalPages) avant de charger plus d'éléments
+              this._loadFilms()
+            }
+          }}
         />
+        {this._displayLoading()}
       </View>
     )
   }
@@ -39,6 +97,15 @@ const styles = StyleSheet.create({
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
 
